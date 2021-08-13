2c8f8eb71fee12f9a132d03ffaf28f2282ed8e5c
Pavlyk01 <81038218+Pavlyk01@users.noreply.github.com>
2021-08-13 19:46:07 +0300

    Add files via upload

---
+++ js/main.js
@@ -0,0 +1,18 @@
+$(function() {
+
+
+
+    $(function() {
+
+        $(".header__burger").click(function() {
+            $('.rightside-menu').toggleClass('menu-up');
+        });
+    });
+    $(function() {
+
+        $(".header__burger-menu").click(function() {
+            $('.rightside-menu').toggleClass('menu-up');
+        });
+    });
+
+})
